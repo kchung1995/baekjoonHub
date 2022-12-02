05b8a6390351218ab976b3c744e7cdafc2102f4b
Kuyho Chung <kchung1995@gmail.com>
2022-12-02 20:12:19 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 12 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -1,19 +1,21 @@
-#include "bits/stdc++.h"
-
+#include <iostream>
 using namespace std;
+#define MAX_N 100001
 
-int arr[100001];
-bool checked[100001];
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    long long n, ans;
+long long n;
+int arr[MAX_N];
+bool checked[MAX_N] = {0, };
+
+void init() {
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    ans = n * (n + 1) / 2;
+}
+
+long long simulate() {
+    long long ans = n * (n + 1) / 2;
     int lt = 0;
     int rt = 1;
     checked[arr[lt]] = true;
@@ -21,107 +23,17 @@ int main() {
         if (checked[arr[rt]]){
             checked[arr[lt]] = false;
             lt++;
-            ans -= n-rt;
+            ans -= n - rt;
         }else {
             checked[arr[rt]] = true;
             rt++;
         }
     }
-    cout << ans;
+    return ans;
 }
 
-/*
-#include "bits/stdc++.h"
-
-using namespace std;
-
-int arr[100001];
-bool checked[100001];
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    long long n, ans;
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    ans = n * (n + 1) / 2;
-    int lt = 0;
-    int rt = 1;
-    checked[arr[lt]] = true;
-    while (rt < n) {
-        if (checked[arr[rt]]){
-            checked[arr[lt]] = false;
-            lt++;
-            ans -= n-rt;
-        }else {
-            checked[arr[rt]] = true;
-            rt++;
-        }
-    }
-    cout << ans;
+    init();
+    cout << simulate();
+    return 0;
 }
-#include "bits/stdc++.h"
-
-using namespace std;
-
-int arr[100001];
-bool checked[100001];
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    long long n, ans;
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    ans = n * (n + 1) / 2;
-    int lt = 0;
-    int rt = 1;
-    checked[arr[lt]] = true;
-    while (rt < n) {
-        if (checked[arr[rt]]){
-            checked[arr[lt]] = false;
-            lt++;
-            ans -= n-rt;
-        }else {
-            checked[arr[rt]] = true;
-            rt++;
-        }
-    }
-    cout << ans;
-}
-#include "bits/stdc++.h"
-
-using namespace std;
-
-int arr[100001];
-bool checked[100001];
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    long long n, ans;
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    ans = n * (n + 1) / 2;
-    int lt = 0;
-    int rt = 1;
-    checked[arr[lt]] = true;
-    while (rt < n) {
-        if (checked[arr[rt]]){
-            checked[arr[lt]] = false;
-            lt++;
-            ans -= n-rt;
-        }else {
-            checked[arr[rt]] = true;
-            rt++;
-        }
-    }
-    cout << ans;
-}
-*/
