89908e147f94a50b8eed27e4e9e6f7e4f7668985
Kuyho Chung <kchung1995@gmail.com>
2023-03-05 23:45:28 +0900

    [Bronze I] Title: 단어 공부, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -1,46 +1,34 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 using namespace std;
 
 int main() {
-    
-    int alphabet[26] = {0, };
-    string input;
-    
-    cin >> input;
-    
-    for (int i = 0; i < input.size(); i++) {
-        if ((int)input[i] - 97 < 0) //대문자임을 의미 {
-        {
-            int temp = input[i] - 65;
-            alphabet[temp]++;
-        }
-        else {                  //소문자
-            int temp = input[i] - 97;
-            alphabet[temp]++;
-        }
-    }
-
-    
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string x;
+    int alp[26];
     int max = 0;
-    int max_counter = 0;
-    int trigger = 1;
-    for (int i = 0; i < 26; i++) {
-        if (alphabet[i] > max) {
-            max = alphabet[i];
-            max_counter = i;
-            trigger = 0;
-        }
-        else if (alphabet[i] == max)
-            trigger = 1;
+    char ans;
+    for (int i = 0; i < sizeof(alp) / sizeof(int); i++) {
+        alp[i] = 0;
     }
-    
-    if (trigger == 1)
-        cout << '?';
-    else
-        cout << (char)(max_counter + 65);
+    cin >> x;
+    for (int i = 0; i < x.size(); i++) {
+        if (x[i] >= 'a') {
+            alp[x[i] - 'a']++;
+        }
+        else alp[x[i] - 'A'] ++;
+    }
 
-    
-    
+    for (int i = 0; i < sizeof(alp) / sizeof(int); i++) {
+        if (alp[i] == max) {
+            ans = '?';
+        }
+        else if (alp[i] > max) {
+            max = alp[i];
+            ans = 'A' + i;
+        }
+    }
+    cout << ans;
     return 0;
 }
